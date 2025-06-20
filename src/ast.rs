ef20313fd64ef0b7fde3e8c37838f55963df5449
Eric E. <e.e@e.e>
2025-06-20 13:05:07 +0200

    function ast

--- src/ast.rs
+++ src/ast.rs
@@ -7,13 +7,11 @@ pub trait Expr {
 pub struct NumberExprAst {
     val: f64,
 }
-
 impl NumberExprAst {
     pub fn new(val: f64) -> NumberExprAst {
         NumberExprAst { val }
     }
 }
-
 impl Expr for NumberExprAst {
     fn print(&self, treeprinter: &mut TreePrinter, indent_lvl: i32, depth: i32) {
         treeprinter.add_print_item(self.val.to_string(), depth, indent_lvl);
@@ -25,7 +23,6 @@ pub struct BinaryExprAst {
     lhs: Box<dyn Expr>,
     rhs: Box<dyn Expr>,
 }
-
 impl BinaryExprAst {
     pub fn new(op: String, lhs: Box<dyn Expr>, rhs: Box<dyn Expr>) -> BinaryExprAst {
         BinaryExprAst { op, lhs, rhs }
@@ -37,5 +34,55 @@ impl Expr for BinaryExprAst {
 
         self.lhs.print(treeprinter, indent_lvl - 1, depth + 1);
         self.rhs.print(treeprinter, indent_lvl + 1, depth + 1);
+    }
+}
+
+pub struct VariableExprAst {
+    name: String,
+}
+impl VariableExprAst {
+    pub fn new(name: String) -> VariableExprAst {
+        VariableExprAst { name }
+    }
+}
+impl Expr for VariableExprAst {
+    fn print(&self, treeprinter: &mut TreePrinter, indent_lvl: i32, depth: i32) {
+        treeprinter.add_print_item(self.name.clone(), depth, indent_lvl);
+    }
+}
+
+pub struct FunctionCallExprAst {
+    callee: String,
+    args: Vec<Box<dyn Expr>>,
+}
+impl FunctionCallExprAst {
+    pub fn new(callee: String, args: Vec<Box<dyn Expr>>) -> FunctionCallExprAst {
+        FunctionCallExprAst { callee, args }
+    }
+}
+impl Expr for FunctionCallExprAst {
+    fn print(&self, treeprinter: &mut TreePrinter, indent_lvl: i32, depth: i32) {
+        // TODO args
+        treeprinter.add_print_item(self.callee.clone(), depth, indent_lvl);
+    }
+}
+
+pub struct PrototypeAst {
+    name: String,
+    args: Vec<String>,
+}
+impl PrototypeAst {
+    pub fn new(name: String, args: Vec<String>) -> PrototypeAst {
+        PrototypeAst { name, args }
+    }
+}
+
+pub struct FunctionAst {
+    proto: PrototypeAst,
+    body: Box<dyn Expr>,
+}
+impl FunctionAst {
+    pub fn new(proto: PrototypeAst, body: Box<dyn Expr>) -> FunctionAst {
+        FunctionAst { proto, body }
     }
 }
